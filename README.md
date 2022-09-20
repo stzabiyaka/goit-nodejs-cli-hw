46249861c88d25249ce8ea904f2bde52d145c428
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-09-20 16:25:26 +0300

    functions created

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# goit-nodejs-cli-hw
+# goit node.js CLI homework
+
+Screenshots:
+
+list: https://monosnap.com/file/4uNgFUA46LDIdS1B86nyuWGniZIgsQ
+
+get: https://monosnap.com/file/MuMD7QpmZ65XtIgKHfRlF0e7NEa1zF
+
+add: https://monosnap.com/file/mVtKGuw3PWe0RLIrEPbL5EsbsZRqOz
+
+remove: https://monosnap.com/file/iIrAnw6KiWhET0mDEi1hRAais9KWOr
---
+++ contacts.js
@@ -0,0 +1,78 @@
+const fs = require('fs/promises');
+const path = require('path');
+
+const contactsPath = path.resolve('./db/contacts.json');
+
+async function listContacts() {
+  try {
+    const result = await getContacts();
+    if (!result || result.length === 0) {
+      return console.log('Contacts list is empty.');
+    }
+    console.table(result);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const result = await getContacts();
+    const contact = result.find(({ id }) => id === contactId);
+    if (!contact) {
+      return console.log('No such contact found.');
+    }
+    console.log('Contact details:');
+    console.table(contact);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const result = await getContacts();
+    const isIdExist = result.some(contact => contact.id === contactId);
+    if (!isIdExist) {
+      return console.log('No such contact found, nothing was removed.');
+    }
+    const updatedContacts = JSON.stringify(result.filter(({ id }) => id !== contactId));
+    await fs.writeFile(contactsPath, updatedContacts, 'utf8');
+    console.log('Contact successfully removed.');
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const result = await getContacts();
+    const isContactExist = result.some(contact => contact.name === name);
+    if (isContactExist) {
+      return console.log(`Contact ${name} is already in the list.`);
+    }
+    const id = (
+      Math.max.apply(
+        null,
+        result.map(({ id }) => Number(id))
+      ) + 1
+    ).toString();
+    const updatedContacts = JSON.stringify([...result, { id, name, email, phone }]);
+    await fs.writeFile(contactsPath, updatedContacts, 'utf8');
+    console.log('Contact successfully added:');
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+async function getContacts() {
+  return JSON.parse(await fs.readFile(contactsPath, 'utf8'));
+}
+
+module.exports = {
+  contactsPath,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
